b363a47ed7990073dccc2d09345bd5af97f3eaa0
talalmajali <t.majali93@gmail.com>
2018-07-03 17:41:34 +0300

    fix for timeToShow

--- index.js
+++ index.js
@@ -131,16 +131,16 @@ class CountDown extends React.Component {
     const {days, hours, minutes, seconds} = this.getTimeLeft();
     const newTime = sprintf('%02d:%02d:%02d:%02d', days, hours, minutes, seconds).split(':');
     const Component = this.props.onPress ? TouchableOpacity : View;
-    const data = timeToShow.map((val, i) => {
-      return _.includes(timeToShow, val) ? this.renderDoubleDigits(this.props[`label${val}`], newTime[i]) : null
-    });
 
     return (
       <Component
         style={styles.timeCont}
         onPress={this.props.onPress}
       >
-        {data}
+        {_.includes(timeToShow, 'D') ? this.renderDoubleDigits(this.props['labelD'], newTime[0]) : null}
+        {_.includes(timeToShow, 'H') ? this.renderDoubleDigits(this.props['labelH'], newTime[1]) : null}
+        {_.includes(timeToShow, 'M') ? this.renderDoubleDigits(this.props['labelM'], newTime[2]) : null}
+        {_.includes(timeToShow, 'S') ? this.renderDoubleDigits(this.props['labelS'], newTime[3]) : null}
       </Component>
     );
   };
