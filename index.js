3955f3e2c0e766b19bb69eaa1cbda5de844aa6c8
Albi Fauzi <32414473+binotby@users.noreply.github.com>
2022-10-13 01:15:36 +0700

    Fix deprecated AppState.removeEventListener

--- index.js
+++ index.js
@@ -51,12 +51,12 @@ class CountDown extends React.Component {
   }
 
   componentDidMount() {
-    AppState.addEventListener('change', this._handleAppStateChange);
+    this.appStateSubscription = AppState.addEventListener('change', this._handleAppStateChange);
   }
 
   componentWillUnmount() {
     clearInterval(this.timer);
-    AppState.removeEventListener('change', this._handleAppStateChange);
+    this.appStateSubscription.remove();
   }
 
   componentDidUpdate(prevProps, prevState) {
